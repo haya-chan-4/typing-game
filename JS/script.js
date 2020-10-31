011d876f6ffd6bb3b913e3d7878d1f6fe831dd84
Hayata <hayata8812@gmail.com>
2020-10-31 16:37:45 +0900

    タイピングゲーム

---
+++ JS/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+{
+  function setWord() {
+    word = words.splice(Math.floor(Math.random() * words.length), 1)[0];
+    target.textContent = word;
+    location = 0;
+  }
+
+  const words = [
+    'red', 'green', 'blue', 'purple', 'black', 'white'
+  ]
+  let word;
+  let location = 0;
+  let startTime;
+  let isPlaying = false;
+  const target = document.getElementById('target');
+
+
+  document.addEventListener('click', () => {
+    if(isPlaying === true){
+      return;
+    }
+    isPlaying = true;
+    startTime =Date.now();
+    setWord();
+  });
+
+  document.addEventListener('keydown',(e) => {
+    if(e.key !== word[location]){
+      return;
+    }
+    if(e.key === word[location]){
+      location++;
+      target.textContent = '_'.repeat(location) + word.substring(location);
+    }
+
+    if (location === word.length) {
+      if(words.length === 0) {
+        const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
+        const result =document.getElementById('result');
+        result.textContent = `finished ${elapsedTime} seconds`;
+        return;
+      }
+      setWord();
+    }
+  });
+}
